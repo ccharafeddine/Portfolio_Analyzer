dd963c423e6ed1ad3b67ba4bf092f83de6567c46
Chafic Charafeddine <85909220+ccharafeddine@users.noreply.github.com>
2025-12-01 21:38:03 -0600

    Download button update

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ if os.path.exists(OUTPUT_DIR):
         zip_buffer.seek(0)
 
         st.download_button(
-            label="⬇️ Download all outputs as ZIP",
+            label="⬇️ Download all outputs as ZIP ⬇️",
             data=zip_buffer,
             file_name="outputs.zip",
             mime="application/zip",
